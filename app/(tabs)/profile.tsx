8dd9452f2e5e4d825844079b7298a19cfa29aaa2
Mudit Agrawal <muditagarwalgkp@gmail.com>
2025-05-18 21:42:09 +0530

    Profile - basic UI

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,15 +1,171 @@
+import Header from "@/components/Header";
 import ScreenWrapper from "@/components/ScreenWrapper";
+import Typo from "@/components/Typo";
+import { colors, radius, spacingX, spacingY } from "@/constants/theme";
+import { useAuth } from "@/context/authContext";
+import { getProfileImage } from "@/services/imageService";
+import { accountOptionType } from "@/types";
+import { verticalScale } from "@/utils/styling";
+import { Image } from "expo-image";
+import * as Icons from "phosphor-react-native";
 import React from "react";
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+import Animated, { FadeInDown } from "react-native-reanimated";
 
 const Profile = () => {
+  const { user } = useAuth();
+
+  const accountOptions: accountOptionType[] = [
+    {
+      title: "Edit Profile",
+      icon: <Icons.User size={26} color={colors.white} weight="fill" />,
+      bgColor: "#6366f1",
+      routeName: "",
+    },
+    {
+      title: "Settings",
+      icon: <Icons.GearSix size={26} color={colors.white} weight="fill" />,
+      bgColor: "#059669",
+      // routeName: "",
+    },
+    {
+      title: "Privacy Policy",
+      icon: <Icons.Lock size={26} color={colors.white} weight="fill" />,
+      bgColor: colors.neutral600,
+      // routeName: "",
+    },
+    {
+      title: "Logout",
+      icon: <Icons.Power size={26} color={colors.white} weight="fill" />,
+      bgColor: "#e11d48",
+      // routeName: "",
+    },
+  ];
   return (
     <ScreenWrapper>
-      <Text>Profile</Text>
+      <View style={styles.container}>
+        <Header title="Profile" style={{ marginVertical: spacingY._10 }} />
+
+        {/* User Info */}
+        <View style={styles.userInfo}>
+          {/* Avatar */}
+          <Image
+            source={getProfileImage(user?.image)}
+            style={styles.avatar}
+            contentFit="cover"
+            transition={100}
+          />
+
+          {/* name n email */}
+          <View style={styles.nameContainer}>
+            <Typo size={24} fontWeight={600} color={colors.neutral100}>
+              {user?.name}
+            </Typo>
+            <Typo size={15} color={colors.neutral400}>
+              {user?.email}
+            </Typo>
+          </View>
+        </View>
+
+        {/* account options */}
+        <View style={styles.accountOptions}>
+          {accountOptions.map((item, index) => {
+            return (
+              <Animated.View
+                key={item.title}
+                style={styles.listItem}
+                entering={FadeInDown.delay(index * 50)
+                  .springify()
+                  .damping(14)}
+              >
+                <TouchableOpacity style={styles.flexRow}>
+                  <View
+                    style={[
+                      styles.listIcon,
+                      { backgroundColor: item?.bgColor },
+                    ]}
+                  >
+                    {item.icon && item.icon}
+                  </View>
+                  <Typo size={16} style={{ flex: 1 }} fontWeight={500}>
+                    {item.title}
+                  </Typo>
+                  <Icons.CaretRight
+                    size={verticalScale(20)}
+                    weight="bold"
+                    color={colors.white}
+                  />
+                </TouchableOpacity>
+              </Animated.View>
+            );
+          })}
+        </View>
+      </View>
     </ScreenWrapper>
   );
 };
 
 export default Profile;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: spacingX._20,
+  },
+  userInfo: {
+    marginTop: verticalScale(30),
+    alignItems: "center",
+    gap: spacingY._20,
+  },
+  avatarContainer: {
+    position: "relative",
+    alignSelf: "center",
+  },
+  avatar: {
+    alignSelf: "center",
+    backgroundColor: colors.neutral300,
+    height: verticalScale(135),
+    width: verticalScale(135),
+    borderRadius: 200,
+  },
+  editIcon: {
+    position: "absolute",
+    bottom: 5,
+    right: 8,
+    borderRadius: 50,
+    backgroundColor: colors.neutral100,
+    shadowColor: colors.black,
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 10,
+    elevation: 4,
+    padding: 5,
+  },
+  nameContainer: {
+    gap: verticalScale(5),
+    alignItems: "center",
+  },
+  listIcon: {
+    height: verticalScale(44),
+    width: verticalScale(44),
+    backgroundColor: colors.neutral500,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: radius._15,
+    borderCurve: "continuous",
+  },
+  listItem: {
+    marginBottom: verticalScale(15),
+  },
+  accountOptions: {
+    marginTop: spacingY._35,
+  },
+  flexRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: spacingX._10,
+  },
+});
