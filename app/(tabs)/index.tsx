fa5c46fea0f543f5e09f59e7ef4540c3f9d83460
Mudit Agrawal <muditagarwalgkp@gmail.com>
2025-05-18 17:56:58 +0530

    Custom tab bars

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,12 @@
 import Button from "@/components/Button";
+import ScreenWrapper from "@/components/ScreenWrapper";
 import Typo from "@/components/Typo";
 import { auth } from "@/config/firebase";
 import { colors } from "@/constants/theme";
 import { useAuth } from "@/context/authContext";
 import { signOut } from "firebase/auth";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 
 const Home = () => {
   const { user } = useAuth();
@@ -18,12 +19,12 @@ const Home = () => {
   };
 
   return (
-    <View>
-      <Text>Home</Text>
+    <ScreenWrapper>
+      <Typo>Home</Typo>
       <Button onPress={handleLogout}>
         <Typo color={colors.black}>Log Out</Typo>
       </Button>
-    </View>
+    </ScreenWrapper>
   );
 };
 
